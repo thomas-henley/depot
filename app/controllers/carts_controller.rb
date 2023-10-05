7e87c2a84ccc2b6c64c0ca6fa6802a06d5f6821c
Thomas Henley <thomas.e.henley@gmail.com>
2023-10-05 08:18:37 -0400

    add websocket functionality

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,6 +54,12 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          :cart,
+          partial: 'layouts/cart'
+        )
+      end
       format.html { redirect_to store_index_url, notice: "Your cart is currently empty." }
       format.json { head :no_content }
     end
